3907185af03f619a6b1c42a75130919cd0bef176
김정민 <kjm3495@naver.com>
2023-03-16 11:14:16 +0900

    feat : 초기설정

---
+++ backend/A708_backend/src/main/java/com/assc/dolbot/DolbotApplication.java
@@ -0,0 +1,13 @@
+package com.assc.dolbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolbotApplication.class, args);
+	}
+
+}

09c0fc4f9156f7a20c1a8a93d7f549e880b86148
kdw3238 <kdw3238@naver.com>
2023-03-27 14:40:42 +0900

    feat : proxy server 이벤트 작성

--- backend/A708_backend_proxyserver/sio_server.py
+++ backend/A708_backend_proxyserver/sio_server.py
@@ -8,14 +8,17 @@ robots = {};
 sio = socketio.Server()
 app = socketio.WSGIApp(sio)
 
+# 누군가 연결했을 때 호출
 @sio.event
 def connect(sid, environ):
     print('새로운 클라이언트 접속:', sid)
 
+# 누군가 연결을 해제 했을 때 호출
 @sio.event
 def disconnect(sid):
     print('클라이언트가 연결 해제:', sid)
 
+# 로봇의 정보를 등록하는 함수
 @sio.event
 def init_robot(sid, data):
     print(data)
@@ -23,6 +26,7 @@ def init_robot(sid, data):
     robots[str(dict['id'])] = sid
     print('robot-sid 매핑:', dict['id'], robots[str(dict['id'])])
 
+# 유저의 정보를 등록하는 함수
 @sio.event
 def init_user(sid, data):
     print(data)
@@ -30,17 +34,42 @@ def init_user(sid, data):
     users[str(dict['id'])] = sid
     print('user-sid 매핑:', dict['id'], sid)
 
+# 날씨의 상태를 유저에게 전달하는 함수 EM -> FE
 @sio.event
 def weather_status(sid, data):
     dict = json.loads(data)
     print(data)
     sio.emit('weather_status', data, to=users[str(dict['to'])])
 
+# 기기의 제어를 로봇에게 요청하는 함수 FE -> EM
 @sio.event
 def appliance_status(sid, data):
     dict = json.loads(data)
     print(data)
     sio.emit('appliance_status', data, to=robots[str(dict['to'])])
+
+# 집의 기기들의 상태를 유저에게 전달하는 함수 EM -> FE
+@sio.event
+def home_status(sid, data):
+    dict = json.loads(data)
+    print(data)
+    sio.emit('home_status', data, to=users[str(dict['to'])])
+
+# 스케줄의 변동을 로봇에게 알리는 함수 FE -> EM
+@sio.event
+def schedule(sid, data):
+    dict = json.loads(data)
+    print(data)
+    sio.emit('schedule',data, to=robots[str(dict['to'])])
+
+# 제어 일정 응급에 대한 알림 메시지 EM -> FE
+@sio.event
+def toast(sid, data):
+    dict = json.loads(data)
+    print(data)
+    sio.emit('toast',data,to=users[str(dict['to'])])
+
+
 
 @sio.event
 def robot_message(sid, data):  # sid는 socket의 id

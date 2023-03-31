45a1cc87690e09732085c7f2ec39f0f7eb37c70f
김정민 <kjm3495@naver.com>
2023-03-31 16:04:42 +0900

    Revert "Merge branch 'BE/feature/schedule-info' into 'master'"
    This reverts merge request !22

--- backend/A708_backend_springboot/src/main/java/com/assc/dolbot/service/UserInfoService.java
+++
@@ -1,8 +0,0 @@
-package com.assc.dolbot.service;
-
-import com.assc.dolbot.dto.UserInfoDto;
-
-public interface UserInfoService {
-	// 사용자 로그인, 회원가입
-	public UserInfoDto loginUserInfo(UserInfoDto userInfoDto) throws Exception;
-}
